3e77ed72016e80cd3d02fa33b20a2febee01646a
shlomit_gila <shlomit.gila@mail.huji.ac.il>
2022-06-15 16:50:15 +0300

    added script to find variable sequence

--- var_rig.py
+++ var_rig.py
@@ -12,10 +12,16 @@ except ValueError:
     valid = False
     continue
 
+# find sequences
+aa_seq =
+cdr1_seq =
+cdr2_seq =
+cdr3_seq =
+
 # get the cdrs locations according to IMGT numbering
-cdr1_start, cdr1_end = .find(cdr1)
-cdr2_start, cdr2_end = ""
-cdr3_start, cdr3_end = ""
+cdr1_start, cdr1_end = aa_seq.find(cdr1_seq)
+cdr2_start, cdr2_end = aa_seq.find(cdr2_seq)
+cdr3_start, cdr3_end = aa_seq.find(cdr3_seq)
 
 # change heavy numbering to Chothia
     cdr1_end -= 1
@@ -36,18 +42,19 @@ sp.run(f"{get_frag_chain} ref_renumber.pdb H {cdr3_start+1} {cdr3_end} > ref_cdr
 
 # get ab model cdrs
 sp.run(f"{get_frag_chain} model_tr.pdb H {cdr1_start+1} {cdr1_end} > model_cdr1.pdb", shell=True, stdout=sp.DEVNULL,stderr=sp.DEVNULL)
-sp.run(f"{get_frag_chain} {chain_model_t} {letter} {cdr2_start+1} {cdr2_end} > ram_cdr2_{chain}.pdb", shell=True, stdout=sp.DEVNULL,stderr=sp.DEVNULL)
-sp.run(f"{get_frag_chain} {chain_model_t} {letter} {cdr3_start+1} {cdr3_end} > ram_cdr3_{chain}.pdb", shell=True, stdout=sp.DEVNULL,stderr=sp.DEVNULL)
+sp.run(f"{get_frag_chain} model_tr.pdb H {cdr2_start+1} {cdr2_end} > ram_cdr2.pdb", shell=True, stdout=sp.DEVNULL,stderr=sp.DEVNULL)
+sp.run(f"{get_frag_chain} model_tr.pdb H {cdr3_start+1} {cdr3_end} > ram_cdr3.pdb", shell=True, stdout=sp.DEVNULL,stderr=sp.DEVNULL)
 
 # calculate cdrs rmsd
 cdr1_rmsd = float(sp.run(f"{rmsd_prog} ref_cdr1.pdb model_cdr1.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
-cdr2_rmsd = float(sp.run(f"{rmsd_prog} ref_cdr2_{chain}.pdb ram_cdr2_{chain}.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
-cdr3_rmsd = float(sp.run(f"{rmsd_prog} ref_cdr3_{chain}.pdb ram_cdr3_{chain}.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
+cdr2_rmsd = float(sp.run(f"{rmsd_prog} ref_cdr2.pdb model_cdr2.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
+cdr3_rmsd = float(sp.run(f"{rmsd_prog} ref_cdr3.pdb model_cdr3.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
 
+print(cdr1_rmsd, cdr2_rmsd, cdr3_rmsd)
 
 
 # calculate frame rmsd
-n = len(sequence)
-n1, n2, n3 = (cdr1_end - cdr1_start ), (cdr2_end - cdr2_start ), (cdr3_end - cdr3_start )
-m = n - n1 - n2 - n3
-fr_rmsd = (((rmsd_all_fr*2)*n - (cdr1_rmsd2)*n1 - (cdr2_rmsd2)*n2 - (cdr3_rmsd2)*n3) / m) * 0.5
+# n = len(sequence)
+# n1, n2, n3 = (cdr1_end - cdr1_start ), (cdr2_end - cdr2_start ), (cdr3_end - cdr3_start )
+# m = n - n1 - n2 - n3
+# fr_rmsd = (((rmsd_all_fr*2)*n - (cdr1_rmsd2)*n1 - (cdr2_rmsd2)*n2 - (cdr3_rmsd2)*n3) / m) * 0.5
