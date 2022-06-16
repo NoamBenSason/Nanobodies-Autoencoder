c958b1a8a04cd1a7bd5a1d1239872c76aaac5cbe
avishaiElmakies <36810152+avishaiElmakies@users.noreply.github.com>
2022-06-16 21:11:31 +0300

    logo fix conflict

--- sequence_logo_files/logo_maker.py
+++ sequence_logo_files/logo_maker.py
@@ -29,22 +29,6 @@ def main():
                    "V": "#5C045E",
                    "X": "#080008", "-": "#080008"}
     logo = lm.Logo(df=crp_df,
-=======
-
-def main():
-    # read prob matrix of specific model architecture and create data frame
-    prob_df = pd.read_csv("prob_csv_1dlf_model_2.csv")
-    prob_df = prob_df.drop(["pos"], axis=1)
-
-    # create and style logo
-    colors_dict = {"A":"#FEA89A", "C":"#FCE59C", "D":"#C0FE9A", "E":"#9CFCD7",
-                   "F":"#9DD9FB", "G":"#B49FF9", "H":"#FA9CFC", "I":"#FE30CD",
-                   "K":"#9231FD", "L":"#3680F8", "M":"#19F7A8", "N":"#9EFA16",
-                   "P":"#FB8815", "Q":"#AD5803", "R":"#9AA907", "S":"#06AA2D",
-                   "T":"#0587AB", "W":"#1202AE", "Y":"#A808AC", "V":"#5C045E",
-                   "X":"#080008", "-":"#080008"}
-    logo = lm.Logo(df=prob_df,
->>>>>>> bc24d7064e6f67fccf3594a44c751ac3944baf37
                    font_name='Hobo Std',
                    fade_below=0,
                    shade_below=0,
