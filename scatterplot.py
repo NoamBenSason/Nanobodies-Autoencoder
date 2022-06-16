abc6b5d546be46c2dfde4aec5ec3d3717470ecba
shlomit_gila <shlomit.gila@mail.huji.ac.il>
2022-06-16 13:08:40 +0300

    added scatterplot script

---
+++ scatterplot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+def create_mse_scatterplot(sequence_vec, structure_vec):
+    """
+    Creates a scatter plot of samples where X axis is the MSE structure and the
+     Y axis is the MSE sequence
+    :param sequence_vec: A vector of size 1 X 50 of the MSE of the sequence for
+     each sample.
+    :param structure_vec: A vector of size 1 X 50 of the MSE of the structure
+    for each sample.
+    :return:
+    """
+
+    return
