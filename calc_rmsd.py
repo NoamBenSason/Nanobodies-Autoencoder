1a10f76d2f7dc64482508069aa2ce5033daa0f3c
shlomit_gila <shlomit.gila@mail.huji.ac.il>
2022-06-16 17:11:00 +0300

    added file

--- calc_rmsd.py
+++ calc_rmsd.py
@@ -1,5 +1,5 @@
 import fnmatch
-
+import pandas as pd
 from var_RMSD import calc
 from os import listdir
 from os.path import isfile,join
@@ -8,9 +8,16 @@ model_list = ["rmsd_variable_parts/attn_out_test_pdbs", "rmsd_variable_parts/mul
 files = fnmatch.filter(listdir("NbTestSet"), "*.pdb")
 test_samples_list = [f for f in files]
 if __name__ == '__main__':
+    df = pd.DataFrame(columns=("model", "sample", "cdr1", "cdr2", "cdr3"))
+    i = 0
     for model in model_list:
         for sample in test_samples_list:
             ref_pdb = 'NbTestSet/' + sample
             predicted_pdb = model + '/pred_' + sample
             print(ref_pdb, predicted_pdb)
-            calc(ref_pdb, predicted_pdb)
+            cdr1, cdr2, cdr3 = calc(ref_pdb, predicted_pdb)
+            df.loc[i] = [model, sample, cdr1, cdr2, cdr3]
+            i += 1
+    df.to_csv("RMSD_results.csv", encoding="utf-8")
+
+
