8bbc56b6ef2e759c420b076a41435c7fd0bb5e52
Noam Ben Sason <75226397+NoamBenSason@users.noreply.github.com>
2022-06-16 20:17:24 +0300

    remove two annoying files

--- calc_rmsd.py
+++ calc_rmsd.py
@@ -4,13 +4,19 @@ from var_RMSD import calc
 from os import listdir
 from os.path import isfile,join
 
+"""
+this script calcs the RMSD of the CDRS for our predicted models
+"""
+
+
 model_list = ["rmsd_variable_parts/attn_out_test_pdbs", "rmsd_variable_parts/multi_attention_out_test_pdbs", "rmsd_variable_parts/original_out_test_pdbs"]
 files = fnmatch.filter(listdir("NbTestSet"), "*.pdb")
-test_samples_list = [f for f in files]
+test_samples_list = [f for f in files if "2vxv" not in f and "2w60" not in f]
 if __name__ == '__main__':
     df = pd.DataFrame(columns=("model", "sample", "cdr1", "cdr2", "cdr3"))
     i = 0
     for model in model_list:
+        a=1
         for sample in test_samples_list:
             ref_pdb = 'NbTestSet/' + sample
             predicted_pdb = model + '/pred_' + sample
