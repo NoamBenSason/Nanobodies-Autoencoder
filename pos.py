6280ac340bf3f7830f8c49d2236d8934592eb6ca
iris <iris.schodl@mail.huji.ac.il>
2022-06-16 18:22:36 +0300

    positions

---
+++ pos.py
@@ -0,0 +1,23 @@
+import fnmatch
+import pandas as pd
+from var_RMSD import calc
+from os import listdir
+from os.path import isfile,join
+
+model_list = ["rmsd_variable_parts/attn_out_test_pdbs", "rmsd_variable_parts/multi_attention_out_test_pdbs", "rmsd_variable_parts/original_out_test_pdbs"]
+files = fnmatch.filter(listdir("NbTestSet"), "*.pdb")
+test_samples_list = [f for f in files]
+if __name__ == '__main__':
+    df = pd.DataFrame(columns=("model", "sample", "cdr1", "cdr2", "cdr3"))
+    i = 0
+    for model in model_list:
+        for sample in test_samples_list:
+            ref_pdb = 'NbTestSet/' + sample
+            predicted_pdb = model + '/pred_' + sample
+            print(ref_pdb, predicted_pdb)
+            cdr1_start,cdr1_end, cdr2_start, cdr2_end, cdr3_start, cdr3_end = calc(ref_pdb, predicted_pdb)
+            df.loc[i] = [model, sample, cdr1_start,cdr1_end, cdr2_start, cdr2_end, cdr3_start, cdr3_end]
+            i += 1
+    df.to_csv("RMSD_results_positions.csv", encoding="utf-8")
+
+
