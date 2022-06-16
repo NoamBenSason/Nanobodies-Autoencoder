6c76d79782c7a50542aba1e8c5e6e30e8d7fddc4
shlomit_gila <shlomit.gila@mail.huji.ac.il>
2022-06-16 13:52:18 +0300

    added file

--- scatterplot.py
+++ scatterplot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def create_mse_scatterplot(sequence_vec, structure_vec):
@@ -13,4 +14,21 @@ def create_mse_scatterplot(sequence_vec, structure_vec):
     :return:
     """
 
-    return
+    # Fixing random state for reproducibility
+    np.random.seed(19680801)
+
+    N = 5
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    colors = np.random.rand(N)
+    area = (30 * np.random.rand(N)) ** 2  # 0 to 15 point radii
+
+    plt.scatter(structure_vec, sequence_vec, alpha=0.5)
+    plt.title("MSE of the structure vs MSE of the sequence")
+    plt.legend()
+    plt.xlabel("MSE structure")
+    plt.ylabel("MSE sequence")
+    plt.show()
+
+if __name__ == '__main__':
+    create_mse_scatterplot([0.3, 0.6, 0.9, 0.32, 0.72], [0.46, 0.81, 0.17, 0.36, 0.78])
